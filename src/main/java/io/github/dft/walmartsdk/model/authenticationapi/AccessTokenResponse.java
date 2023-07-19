c9c7d1fb2a5b05bdcaccefb60779243f6002b20f
abhishek.sangani <abhishek.sangani@dakshyafreondtech.com>
2023-07-19 13:05:10 +0530

    EC-7 Implemented refresh access token method Added SnakeCaseStrategy into AccessTokenResponse class Updated WalmartCredentials Pojo

--- src/main/java/io/github/dft/walmartsdk/model/authenticationapi/AccessTokenResponse.java
+++ src/main/java/io/github/dft/walmartsdk/model/authenticationapi/AccessTokenResponse.java
@@ -2,6 +2,8 @@ package io.github.dft.walmartsdk.model.authenticationapi;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
