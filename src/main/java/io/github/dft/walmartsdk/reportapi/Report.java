9f16f1975f8d14a8ca3e207dbaf7a919b6896b03
Hemangi.Chauhan <hemangi.chauhan@dakshyafreondtech.com>
2023-08-23 16:24:20 +0530

    SYNKRON-148
    Created pojo for Report Requests.
    Created method for Get All Report Requests.
    Updated sdk version.

--- src/main/java/io/github/dft/walmartsdk/reportapi/Report.java
+++ src/main/java/io/github/dft/walmartsdk/reportapi/Report.java
@@ -1,13 +1,11 @@
 package io.github.dft.walmartsdk.reportapi;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Report {
 
